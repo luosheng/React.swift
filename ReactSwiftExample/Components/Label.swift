c8766936a3acdda5b1907ca5a7a0421cd88c8bd1
Luo Sheng <luosheng1986@gmail.com>
2016-03-05 23:16:27 +0800

    Create a label component.

---
+++ ReactSwiftExample/Components/Label.swift
@@ -0,0 +1,33 @@
+//
+//  Label.swift
+//  ReactSwift
+//
+//  Created by Luo Sheng on 16/3/5.
+//  Copyright © 2016年 Luo Sheng. All rights reserved.
+//
+
+import UIKit
+import ReactSwift
+
+struct LabelState : StateType {
+}
+
+struct LabelProperty : PropertyType {
+    let text: String
+}
+
+class Label : BaseComponent<LabelState, LabelProperty> {
+    
+    init(property: LabelProperty) {
+        super.init(property: property)
+    }
+    
+    override func render() -> Renderable? {
+        let label = UILabel()
+        if let property = property {
+            label.text = property.text
+        }
+        return label
+    }
+    
+}
